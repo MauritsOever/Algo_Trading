74f551eb85313828ef352878f0b41de46e550edd
Connor <c.m.stevens@student.vu.nl>
2022-03-10 12:37:09 +0100

    added plots and updated calculate_mid to calculate_mids

--- ass3_main.py
+++ ass3_main.py
@@ -10,20 +10,77 @@ class Assignment3():
         self.train_data = pd.read_csv(train_data_file)
         self.test_data = pd.read_csv(test_data_file)
 
-    def fill_na(self, data):
+    def fill_na(self):
 
-        data = data.ffill()
-        data = data.bfill()
+        self.train_data = self.train_data.ffill()
+        self.test_data = self.train_data.bfill()
+
+        self.test_data.ffill()
+        self.test_data.bfill()
 
     def check_order(self, data):
+
         if not np.all(data.sort_values('epochhours').index == data.index):
+
             data = data.sort_values('epochhours')
 
-    def calculate_mid(self, bid, ask):
+    def calculate_mids(self):
 
-        mid = (bid + ask) / 2
+        self.train_data['midDealerQuotes'] = (self.train_data['firm_executable_bid']
+                                              + self.train_data['firm_executable_ask']) / 2
 
-        return mid
+        self.train_data['midMarketEstimate'] = (self.train_data['market_estimate_bid']
+                                              + self.train_data['market_estimate_ask']) / 2
+
+        self.test_data['midDealerQuotes'] = (self.test_data['firm_executable_bid']
+                                              + self.test_data['firm_executable_ask']) / 2
+
+        self.test_data['midDealerQuotes'] = (self.test_data['market_estimate_bid']
+                                              + self.test_data['market_estimate_ask']) / 2
+
+    def generate_plots(self):
+
+        # Create figure.
+        fig, axs = plt.subplots(2, 2)
+
+        fig.suptitle('Epoch Hours')
+
+        axs[0, 0].plot(
+            self.train_data['epochhours'],
+            self.train_data['last_price']
+            )
+
+        axs[0, 1].plot(
+            self.train_data['epochhours'],
+            self.train_data['midDealerQuotes']
+            )
+
+        axs[1, 0].plot(
+            self.train_data['epochhours'],
+            self.train_data['midMarketEstimate']
+            )
+
+        axs[1, 1].plot(
+            self.train_data['epochhours'],
+            self.train_data['last_price']
+            )
+
+        axs[1, 1].plot(
+            self.train_data['epochhours'],
+            self.train_data['last_price']
+            )
+
+        axs[1, 1].plot(
+            self.train_data['epochhours'],
+            self.train_data['midDealerQuotes']
+            )
+
+        axs[1, 1].plot(
+            self.train_data['epochhours'],
+            self.train_data['midMarketEstimate']
+            )
+
+        plt.show()
 
 
 # HERE WE RUN THE CODE
@@ -31,47 +88,26 @@ class Assignment3():
 # Setup filepaths.
 
 # Get current working directory.
-cwd = os.getcwd()
+cwd = os.curdir
 
 # Get data file paths.
 train_data_file = os.path.join(cwd, 'Bond_MidModelTraining.csv')
 test_data_file = os.path.join(cwd, 'Bond_QuoteLive.csv')
-
-# load in and prepare data
-# train_data_file = r'C:\Users\gebruiker\Documents\GitHub\Algo_Trading\Bond_MidModelTraining.csv'
-# test_data_file  = r'C:\Users\gebruiker\Documents\GitHub\Algo_Trading\Bond_QuoteLive.csv'
 
 # Instantiate Assignment3
 
 assignment = Assignment3(train_data_file, test_data_file)
 
 # Fill nans.
-assignment.fill_na(assignment.train_data)
-assignment.fill_na(assignment.test_data)
+assignment.fill_na()
 
 # check if both sets are ordered properly by epochhours
 assignment.check_order(assignment.train_data)
 assignment.check_order(assignment.test_data)
 
-assignment.train_data['midDealerQuotes'] = assignment.calculate_mid(
-    assignment.train_data['firm_executable_bid'],
-    assignment.train_data['firm_executable_ask']
-)
-assignment.train_data['midMarketEstimate'] = assignment.calculate_mid(
-    assignment.train_data['market_estimate_bid'],
-    assignment.train_data['market_estimate_ask'])
+assignment.calculate_mids()
 
-assignment.test_data['midDealerQuotes'] = assignment.calculate_mid(
-    assignment.test_data['firm_executable_bid'],
-    assignment.test_data['firm_executable_ask']
-)
-assignment.test_data['midMarketEstimate'] = assignment.calculate_mid(
-    assignment.test_data['market_estimate_bid'],
-    assignment.test_data['market_estimate_ask'])
-
-print(assignment.train_data.head())
-
-
+assignment.generate_plots()
 # np.all(data_train.index == data_train.sort_values('epochhours').index)
 # np.all(data_test.index == data_test.sort_values('epochhours').index)
 
