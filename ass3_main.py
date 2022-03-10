9f2fd8390edcc17a6360364041085a04095d139e
Connor <c.m.stevens@student.vu.nl>
2022-03-10 11:40:01 +0100

    Added agnostic file paths.

--- ass3_main.py
+++ ass3_main.py
@@ -1,4 +1,5 @@
 # load in appropriate packages
+import os
 import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
@@ -11,18 +12,34 @@ class Assignment3():
 
     def fill_na(self, data):
 
-        data.ffill()
-        data.bfill()
+        data = data.ffill()
+        data = data.bfill()
 
     def check_order(self, data):
         if not np.all(data.sort_values('epochhours').index == data.index):
             data = data.sort_values('epochhours')
 
+    def calculate_mid(self, bid, ask):
+
+        mid = (bid + ask) / 2
+
+        return mid
+
+
 # HERE WE RUN THE CODE
 
+# Setup filepaths.
+
+# Get current working directory.
+cwd = os.getcwd()
+
+# Get data file paths.
+train_data_file = os.path.join(cwd, 'Bond_MidModelTraining.csv')
+test_data_file = os.path.join(cwd, 'Bond_QuoteLive.csv')
+
 # load in and prepare data
-train_data_file = r'C:\Users\gebruiker\Documents\GitHub\Algo_Trading\Bond_MidModelTraining.csv'
-test_data_file  = r'C:\Users\gebruiker\Documents\GitHub\Algo_Trading\Bond_QuoteLive.csv'
+# train_data_file = r'C:\Users\gebruiker\Documents\GitHub\Algo_Trading\Bond_MidModelTraining.csv'
+# test_data_file  = r'C:\Users\gebruiker\Documents\GitHub\Algo_Trading\Bond_QuoteLive.csv'
 
 # Instantiate Assignment3
 
@@ -36,6 +53,23 @@ assignment.fill_na(assignment.test_data)
 assignment.check_order(assignment.train_data)
 assignment.check_order(assignment.test_data)
 
+assignment.train_data['midDealerQuotes'] = assignment.calculate_mid(
+    assignment.train_data['firm_executable_bid'],
+    assignment.train_data['firm_executable_ask']
+)
+assignment.train_data['midMarketEstimate'] = assignment.calculate_mid(
+    assignment.train_data['market_estimate_bid'],
+    assignment.train_data['market_estimate_ask'])
+
+assignment.test_data['midDealerQuotes'] = assignment.calculate_mid(
+    assignment.test_data['firm_executable_bid'],
+    assignment.test_data['firm_executable_ask']
+)
+assignment.test_data['midMarketEstimate'] = assignment.calculate_mid(
+    assignment.test_data['market_estimate_bid'],
+    assignment.test_data['market_estimate_ask'])
+
+print(assignment.train_data.head())
 
 
 # np.all(data_train.index == data_train.sort_values('epochhours').index)
