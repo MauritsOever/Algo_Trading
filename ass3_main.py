4429813fc52c92e0b684c5bbc8750855b67b27ff
MauritsOever <71381155+MauritsOever@users.noreply.github.com>
2022-03-10 11:20:33 +0100

    Update ass3_main.py

--- ass3_main.py
+++ ass3_main.py
@@ -14,12 +14,15 @@ class Assignment3():
         data.ffill()
         data.bfill()
 
+    def check_order(self, data):
+        if not np.all(data.sort_values('epochhours').index == data.index):
+            data = data.sort_values('epochhours')
 
 # HERE WE RUN THE CODE
 
 # load in and prepare data
-train_data_file = 'Bond_MidModelTraining.csv'
-test_data_file  = 'Bond_QuoteLive.csv'
+train_data_file = r'C:\Users\gebruiker\Documents\GitHub\Algo_Trading\Bond_MidModelTraining.csv'
+test_data_file  = r'C:\Users\gebruiker\Documents\GitHub\Algo_Trading\Bond_QuoteLive.csv'
 
 # Instantiate Assignment3
 
@@ -27,20 +30,18 @@ assignment = Assignment3(train_data_file, test_data_file)
 
 # Fill nans.
 assignment.fill_na(assignment.train_data)
-assignment.test_data.fill_na(assignment.test_data)
+assignment.fill_na(assignment.test_data)
 
 # check if both sets are ordered properly by epochhours
+assignment.check_order(assignment.train_data)
+assignment.check_order(assignment.test_data)
 
-if not np.all(data_train.sort_values('epochhours').index == data_train.index):
-    data_train = data_train.sort_values('epochhours', ignore_index=True)
 
-if not np.all(data_test.sort_values('epochhours').index == data_test.index):
-    data_test = data_test.sort_values('epochhours', ignore_index = True)
 
-np.all(data_train.index == data_train.sort_values('epochhours').index)
-np.all(data_test.index == data_test.sort_values('epochhours').index)
+# np.all(data_train.index == data_train.sort_values('epochhours').index)
+# np.all(data_test.index == data_test.sort_values('epochhours').index)
 
-data_train['midDealerQuotes'] = (data_train.firm_executable_bid + data_train.firm_executable_ask) / 2
-data_train['midMarketEstimate'] = (data_train.market_estimate_bid + data_train.market_estimate_ask) / 2
+# data_train['midDealerQuotes'] = (data_train.firm_executable_bid + data_train.firm_executable_ask) / 2
+# data_train['midMarketEstimate'] = (data_train.market_estimate_bid + data_train.market_estimate_ask) / 2
 
-plt.plot(data_train.midDealerQuotes)
+# plt.plot(data_train.midDealerQuotes)
